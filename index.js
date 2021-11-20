cdbab6a31b031489a85abfb025831ebb339c42f7
Zachary Anderson <60018799+xyandzach@users.noreply.github.com>
2021-11-20 11:39:40 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,105 @@
+function changeIcon(button) {
+    switch(button.id) {
+        case "codingButton":
+            if (document.getElementById("codingIcon").src.includes("rsc/codingiconorange.png")) {
+                document.getElementById("codingIcon").src = "rsc/codingicon.png";
+            } else {
+                document.getElementById("codingIcon").src = "rsc/codingiconorange.png";
+            }
+            break;
+        case "fishkeepingButton":
+            if (document.getElementById("fishkeepingIcon").src.includes("rsc/fishiconorange.png")) {
+                document.getElementById("fishkeepingIcon").src = "rsc/fishicon.png";
+            } else {
+                document.getElementById("fishkeepingIcon").src = "rsc/fishiconorange.png";
+            }
+            break;
+        case "gamingButton":
+            if (document.getElementById("gamingIcon").src.includes("rsc/gamingiconorange.png")) {
+                document.getElementById("gamingIcon").src = "rsc/gamingicon.png";
+            } else {
+                document.getElementById("gamingIcon").src = "rsc/gamingiconorange.png";
+            }
+            break;
+    }
+}
+
+function perfChecks() {
+    if (window.innerWidth == 980 || window.innerWidth == 1024) {
+        //mobile/tablet
+        var bgFish = document.getElementById("bgFish");
+        var downArrow = document.getElementById("downArrow");
+        var pageLinkIcons = document.getElementsByClassName("page-links-button-img");
+        var projectTitles = document.getElementsByClassName("project-title");
+        var projectLanguages = document.getElementsByClassName("project-languages");
+        var projectDescription = document.getElementsByClassName("project-description");
+        var projectArrows = document.getElementsByName("sideArrow");
+        var buttons = document.getElementsByClassName("page-links-button");
+        var letsFish = document.getElementById("letsFish");
+        var fishText = document.getElementById("fishText");
+        var fishDescription = document.getElementsByClassName("fish-description");
+        var fishImgs = document.getElementsByClassName("fish-img");
+        var emailMessage = document.getElementsByClassName("email-message");
+        var emailButton = document.getElementsByClassName("email-button");
+        var headerLinks = document.getElementsByClassName("header-links");
+        var footerImg = document.getElementById("footerImg");
+        var footerNav = document.getElementById("footerNav");
+        var socialLinks = document.getElementById("socialLinks");
+        var welcomeLinks = document.getElementsByClassName("welcome-links");
+        var headerLogo = document.getElementById("headerLogo");
+        var pageLinks = document.getElementsByClassName("page-link-icon");
+        for(var i = 0;i <= pageLinks.length-1;i++) {
+            pageLinks[i].style.width = "5%";
+        }
+        pageLinks[0].style.right = "70%";
+        pageLinks[1].style.right = "25%";
+        socialLinks.style.marginLeft = "28.5%";
+        footerNav.style.width = "17%";
+        footerImg.style.visibility = "hidden";
+        emailButton[0].style.paddingRight = "10px";
+        emailButton[0].style.paddingLeft = "10px";
+        emailMessage[0].style.paddingRight = "125px";
+        fishImgs[0].style.width = "75%";
+        fishImgs[1].style.width = "75%";
+        fishDescription[0].style.marginRight = "0";
+        fishDescription[1].style.marginLeft = "0";
+        fishText.style.fontSize = "15pt";
+        letsFish.style.fontSize = "10pt";
+        letsFish.style.marginTop = "35px";
+        for (var i=0;i<buttons.length;i++) {
+            buttons[i].style.maxWidth = "30%";
+        }
+        for (var i=0;i<projectTitles.length;i++) {
+            if(i <= 3) {
+                projectLanguages[i].style.fontSize = "60%";
+                projectArrows[i].style.height = "2%";
+                projectArrows[i].style.paddingTop = "4%";
+                projectDescription[i].style.fontSize = "85%";
+            }
+            projectTitles[i].style.fontSize = "100%";
+        }
+        for (var i=0;i<pageLinkIcons.length;i++) {
+            pageLinkIcons[i].style.height = "2%";
+        }
+
+        bgFish.style.display = "none";
+        downArrow.style.display = "none";
+        for (var i = 0; i <= headerLinks.length - 1;i++) {
+            headerLinks[i].style.width = "20%";
+        }
+        for(var t=0;t<=welcomeLinks.length - 1;t++) {
+            welcomeLinks[t].style.fontSize = "175%";
+        }
+        headerLogo.style.paddingRight = "20%";
+    } else if (window.innerWidth == 1280 && window.innerHeight == 800) {
+        //macbook
+    } else if (window.innerWidth == 1280) {
+        //laptop w/ touch
+    } else if (window.innerWidth == 1440) {
+        //laptop
+    } 
+}
+
+window.onload = function() {
+    perfChecks();
+}
